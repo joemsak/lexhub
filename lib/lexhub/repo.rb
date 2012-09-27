bfac37ad43534c2951c6d0cdf29d0e932112f9aa
Joe Sak <joe@joesak.com>
2012-09-26 23:50:57 -0500

    Rename the memoize variable

--- lib/lexhub/repo.rb
+++ lib/lexhub/repo.rb
@@ -2,7 +2,7 @@ module Lexhub
   class Repo < Base
 
     def commits
-      @results ||= _collect(:commit, 'repos.commits.all')
+      @commits ||= _collect(:commit, 'repos.commits.all')
     end
   end
 end
