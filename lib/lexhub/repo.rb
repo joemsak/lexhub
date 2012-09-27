6ba976060109658a652c1be8e33fe59350f2c07e
Joe Sak <joe@joesak.com>
2012-09-26 21:53:26 -0500

    Refactor

--- lib/lexhub/repo.rb
+++ lib/lexhub/repo.rb
@@ -1,14 +1,32 @@
 module Lexhub
   class Repo
+    attr_accessor :username, :repo_name, :commits
+
     def initialize(username, repo_name)
       @username  = username
       @repo_name = repo_name
+      @commits   = collect_commits
     end
 
-    def commits
-      @commits ||= GITHUB.repos.commits
-                         .all(@username, @repo_name)
-                         .collect(&:commit)
+    private
+    def collect_commits
+      @results ||= []
+      return @results if @results.any?
+      @results = commits_from_all_pages_of_response
+    end
+
+    def commits_from_all_pages_of_response
+      _commits = []
+
+      response.each_page do |page|
+        _commits += page.collect(&:commit)
+      end
+
+      _commits
+    end
+
+    def response
+      @response ||= GITHUB.repos.commits.all(username, repo_name)
     end
   end
 end
