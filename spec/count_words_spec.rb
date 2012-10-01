72578db778d73a5d746ca37e87f14d7bde5cfc4e
Joe Sak <joe@joesak.com>
2012-10-01 11:16:26 -0500

    Remove specific that changes over time

--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -6,7 +6,6 @@ describe 'Counting words in all commit messages' do
     VCR.use_cassette('collect all the words') do
       repo = Lexhub::Repo.new('joemsak', 'lexhub')
 
-      repo.words.keys.count.should == 53
       repo.words['initial'].should == { :count => 1 }
     end
   end
